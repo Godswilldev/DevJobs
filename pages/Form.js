0649b0b22bb05b83fbe1b50a06a7147aab7e746d
Godswill Edet <gimmex1@gmail.com>
2021-07-11 20:36:36 +0100

    initial commit

---
+++ pages/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const Form = () => {
+  return (
+    <div>
+      <h1>form page</h1>
+      <Link href="/">Home page</Link>
+    </div>
+  );
+};
+
+export default Form;
